c3c58f24b0e11565e0648301992dd1dca4d66f2d
oming04 <96978897+oming04@users.noreply.github.com>
2022-03-22 05:43:27 +0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -94,6 +94,12 @@ async def start_command(client: Client, message: Message):
                     InlineKeyboardButton("🔒 Tutup Jangan Kepo", callback_data = "close")
                 ]
             ]
+            [
+                [
+                    InlineKeyboardButton("😊 Kepo Ya?", callback_data = "about"),
+                    InlineKeyboardButton("🔒 Tutup Jangan Kepo", callback_data = "close")
+                ]
+            ]
         )
         await message.reply_text(
             text = START_MSG.format(
