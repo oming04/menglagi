6f41d3ed09fbac8f4dc2223e290dbfe631f19524
oming04 <96978897+oming04@users.noreply.github.com>
2022-03-23 14:07:18 +0800

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : </a>\n○ Source Code : <a href='tg://settings'>Click Here</a></b>",
+            text = f"<b>○ Source Code : <a href='tg://settings'>Click Here</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
